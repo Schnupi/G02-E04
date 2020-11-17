bbd698aeff2062096620dee5b543e72490722f4d
Matthias <matthias.gander@braineet.com>
2020-11-17 14:26:45 +0100

    File structure imporvement

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = () => {
+  return (
+    <div>
+      <button style={{}}>Hello</button>
+    </div>
+  );
+};
+
+Button.propTypes = {};
+
+export default Button;
---
+++ src/pages/Login/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.scss";
+import Button from "../../components/Button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+};
+
+export default App;
