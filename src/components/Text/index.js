ce759157cb55f0e998940bd2ce1f275a84a4f85c
Schnupi <julie.chen@hetic.net>
2020-11-17 15:18:32 +0100

    text style imported

---
+++ src/components/Text/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './style.scss'
+
+const Text = () => {
+  return (
+    <div>
+      <p className="caption-emphasis">Hello</p>
+    </div>
+  );
+};
+
+Text.propTypes = {};
+
+export default Text;
--- src/pages/Login/App.js
+++ src/pages/Login/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./app.scss";
 import Button from "../../components/Button";
+import Text from "../../components/Text";
 
 const App = () => {
   return (
     <div className="App">
-      <Button></Button>
+      <Text></Text>
     </div>
   );
 };
